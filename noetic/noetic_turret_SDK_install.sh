8b9f67f401531b66a63ca41f0f5aa93c2449fdd5
KF <kf@digitalmunition.com>
2023-04-04 21:50:51 -0400

    Fix installer

--- noetic/noetic_turret_SDK_install.sh
+++ noetic/noetic_turret_SDK_install.sh
@@ -1,12 +1,12 @@
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-#sudo apt-get install curl nano
-#sudo apt-get install guvcview
-#curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-#sudo apt update
-#sudo apt-get install ros-noetic-depthai "ros-noetic-depthai*" "ros-noetic-dynamixel*" ros-noetic-desktop-full
-#sudo apt-get install python3-catkin python3-catkin-pkg-modules python3-rosdep python3-rosdep-modules python3-catkin-tools
-#sudo rosdep init
-#rosdep update
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-get install curl nano
+sudo apt-get install guvcview
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt-get install ros-noetic-depthai "ros-noetic-depthai*" "ros-noetic-dynamixel*" ros-noetic-desktop-full
+sudo apt-get install python3-catkin python3-catkin-pkg-modules python3-rosdep python3-rosdep-modules python3-catkin-tools
+sudo rosdep init
+rosdep update
 
 mkdir robotis_turret_ws/src -p
 cd robotis_turret_ws/src
@@ -30,12 +30,13 @@ rm -rf interbotix_ros_arms
 
 cp interbotix_descriptions/urdf/vxxms.urdf.xacro interbotix_ros_turrets/interbotix_ros_xsturrets/interbotix_xsturret_descriptions/urdf/
 
+wget https://github.com/MAVProxyUser/Gelblaster_Wingman/raw/main/CC_BYNCSA_STL_Files/CC_BYNCSA_SurgeXL_quadruped_kit.stl
+mv CC_BYNCSA_SurgeXL_quadruped_kit.stl interbotix_descriptions/meshes/meshes_vxxms/
+
 source /opt/ros/noetic/setup.sh
 cd ~/robotis_turret_ws/
 rosdep install --from-paths src --ignore-src -r -y
 
-wget https://github.com/MAVProxyUser/Gelblaster_Wingman/raw/main/CC_BYNCSA_STL_Files/CC_BYNCSA_SurgeXL_quadruped_kit.stl
-mv CC_BYNCSA_SurgeXL_quadruped_kit.stl src/interbotix_descriptions/meshes/meshes_vxxms/
 wget https://github.com/MAVProxyUser/Step2.1-GelBlaster-Wingman/raw/main/noetic/turret_STL_replace.diff
 cat turret_STL_replace.diff | patch -p0
 cp ./src/interbotix_descriptions/urdf/vxxms.urdf.xacro ./src/interbotix_ros_turrets/interbotix_ros_xsturrets/interbotix_xsturret_descriptions/urdf/vxxms.urdf.xacro
@@ -64,4 +65,3 @@ echo "add the following entries to /etc/profile of ~/.profile :"
 echo "export LD_PRELOAD=/lib/aarch64-linux-gnu/libGLdispatch.so.0:\$LD_PRELOAD"
 echo "source /opt/ros/noetic/setup.sh"
 echo "source /home/ubuntu/robotis_turret_ws/devel/setup.sh"
-
